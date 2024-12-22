6655f5c59d6366dcd1e6e61a20cae5da65bfed83
Matt Giorgio <mgiorgio@mitre.org>
2024-12-21 23:22:32 -0500

    Data collection functional

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/App';
+import Instructions from './pages/instruct';
+import NotFound from './pages/404'
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/instructions' element={<Instructions />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
+    <Home />
   </React.StrictMode>
 );
 
