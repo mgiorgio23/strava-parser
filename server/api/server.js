be7b32968bbdfce39a45604d2b706d60b60ca3d3
Matt Giorgio <mgiorgio@mitre.org>
2025-01-18 12:04:33 -0500

    Changed some commands in the package.jsons

--- server/api/server.js
+++ server/api/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
+console.log(PORT);
 
 const app = express();
 const allowedOrigins = [
@@ -13,21 +14,7 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cors());
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials:true,
-//   optionSuccessStatus:200
-// }));
+
 app.options('*', cors());
 app.set('view engine','ejs')
 
@@ -55,11 +42,6 @@ app.get("/*", function (req, res) {
     }
   )});
 
-  // if (process.env.NODE_ENV !== 'production') {
-  //   app.listen(PORT, () => {
-  //     console.log(`Server listening on port ${PORT}`);
-  //   });
-  // };
 app.listen(PORT, () => console.log("Listening to port " + PORT));
 module.exports = app
 
